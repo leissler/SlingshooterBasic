927fff9e23d90308018e9f125ac5ec21125962ab
Martin Leissler <martin@leissler.net>
2015-05-11 15:56:07 +0200

    Added Slingshot and behavior script

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+
+	void Awake() {
+		Transform launchPointTransform = transform.Find("LaunchPoint");
+		launchPoint = launchPointTransform.gameObject;
+		launchPoint.SetActive(false);
+	}
+
+	void OnMouseEnter() {
+		//print ("Yay, the mouse has entered!");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		//print ("Oh no, the mouse has exited!");
+		launchPoint.SetActive(false);
+	}
+
+}
